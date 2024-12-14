73d06dc36887a946b0f035b1176fb9bd207c7416
artarts36 <temicska99@mail.ru>
2024-12-14 22:15:30 +0300

    chore: simplify

--- scripts/git-init.sh
+++ scripts/git-init.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Init submodules"
 
-if [[ "$CODESPACES" == "true" ]] || [[ "$USE_GIT_ENDPOINTS" == "true" ]]; then
+if [[ "$CODESPACES" == "true" ]] || [[ "$USE_GIT_HTTP_ENDPOINTS" == "true" ]]; then
     echo "Modify codespaces environment"
     git config --global url."https://".insteadOf git://
     git config --global url."https://github.com/".insteadOf git@github.com:
