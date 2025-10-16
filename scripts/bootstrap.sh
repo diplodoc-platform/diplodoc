2d9e671b234c3c24b693d4e73390f08671c38b02
3y3 <3y3@ya.ru>
2025-10-17 00:42:01 +0300

    fix: Fix E2E tests

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,13 +5,15 @@ set -e
 ROOT=$(dirname $0)
 
 . $ROOT/git-init.sh
-. $ROOT/git-head.sh
+. $ROOT/git-apply.sh master
 
 # maybe npm ci?
 echo "Install dependencies"
 npm i
 
-npm run reset -- -u
+npm run reset -- -u -q
 
 echo "Build CLI"
 npx nx build @diplodoc/cli
+
+npm i
